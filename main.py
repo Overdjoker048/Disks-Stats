fe1eec995d5a6c0d1fee04e9c86c7e0c670b5f24
Overdjoker048 <92109090+Overdjoker048@users.noreply.github.com>
2024-09-23 18:44:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from tkinter import *
 def analyse():
     drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
 
+    #Retrieve the list of disks and retrieve the information
     storage = []
     for i in drives:
         try:
             storage.append(psutil.disk_usage(i))
         except PermissionError:
             storage.append("pass")
+
+    #Format the text to display it on the gui
     drives_values = ""
     for i in range(len(storage)):
         if storage[i] != "pass":
@@ -18,9 +21,12 @@ def analyse():
                 drives_values = drives_values + "\n"
             drives_values = f"{drives_values}({drives[i]}): {round(storage[i].used/10**9, 2)}Go/{round(storage[i].total/10**9, 2)}Go"
 
+    #Update the storage display
     drives_display['text'] = drives_values
-    window.after(250, analyse)
+    #Restart the function
+    window.after(200, analyse)
 
+#Create the GUI
 window = Tk()
 window.overrideredirect(True)
 window.config(bg="#1E1E1E")
@@ -28,6 +34,10 @@ window.wm_attributes("-topmost", True)
 drives_display = Label(window, font=("Courrier", 11, "bold"), fg="#252525", bg="white", highlightbackground="#1E1E1E", highlightcolor="#1E1E1E", highlightthickness=2)
 drives_display.pack()
 
+#Allows you to close the page by pressing Ctrl+m
 window.bind("<Control-m>", lambda event: window.destroy())
+#Actualise l affichage des stockages
 window.after(0, analyse)
+
+#Display the GUI
 window.mainloop()
